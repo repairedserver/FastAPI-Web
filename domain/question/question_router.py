7b54dbf6f2a7434946594d900a2df3344e2bab3f
김세현 <dnjfdid14@naver.com>
2022-11-24 18:54:37 +0900

    ⚡️ :: 수정

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from domain.question import question_schema
+from domain.question import question_schema, question_crud
+
 from database import get_db
-from models import Question
 
 router = APIRouter(
     prefix="/api/question",
@@ -11,5 +11,5 @@ router = APIRouter(
 
 @router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    _question_list = question_crud.get_question_list(db)
     return _question_list
