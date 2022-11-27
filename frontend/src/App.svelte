fae43beae2c5b73665a4e76262e1279acaab5f01
김세현 <dnjfdid14@naver.com>
2022-11-27 15:11:59 +0900

    ⚡️ :: 질문 목록 화면

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,19 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import Home from "./routes/Home.svelte"
+  let question_list = []
 
-  const routes = {
-    '/': Home,
+  function get_question_list() {
+    fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
+      response.json().then((json) => {
+        question_list = json
+      })
+    })
   }
+
+  get_question_list()
 </script>
 
-<Router {routes}/>
+<ul>
+  {#each question_list as question}
+    <li>{question.subject}</li>
+  {/each}
+</ul>
