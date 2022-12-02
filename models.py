19d7b0941b4f88177d2f44de54b080398e8fac31
김세현 <dnjfdid14@naver.com>
2022-12-02 15:08:40 +0900

    ⚡️ :: 회원가입

--- models.py
+++ models.py
@@ -4,6 +4,14 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+
 class Question(Base):
     __tablename__ = "question"
 
