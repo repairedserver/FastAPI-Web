32bb0df4f94f9830fc3a3eb6cb674ae744959588
김세현 <dnjfdid14@naver.com>
2022-12-03 13:37:23 +0900

    ⚡️ :: 수정

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from starlette import status
+from datetime import timedelta, datetime
 
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 from database import get_db
 from domain.user import user_crud, user_schema
+from domain.user.user_crud import pwd_context
 
 router = APIRouter(
     prefix="/api/user",
@@ -13,4 +17,8 @@ router = APIRouter(
 
 @router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
 def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_db)):
+    user = user_crud.get_existing_user(db, user_create=_user_create)
+    if user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="이미 존재하는 사용자입니다.")
     user_crud.create_user(db=db, user_create=_user_create)
