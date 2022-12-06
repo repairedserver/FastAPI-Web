ad067a76c864f9e0d49dbeb2a5e7fa9c53097a29
김세현 <dnjfdid14@naver.com>
2022-12-06 17:08:21 +0900

    ⚡️ :: 로그아웃

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,4 +1,4 @@
-from models import Question
+from models import Question, User
 from sqlalchemy.orm import Session
 from datetime import datetime
 
@@ -19,9 +19,10 @@ def get_question(db: Session, question_id: int):
     return question
 
 
-def create_question(db: Session, question_create: QuestionCreate):
+def create_question(db: Session, question_create: QuestionCreate, user: User):
     db_question = Question(subject=question_create.subject,
                            content=question_create.content,
-                           create_date=datetime.now())
+                           create_date=datetime.now(),
+                           user=user)
     db.add(db_question)
     db.commit()
