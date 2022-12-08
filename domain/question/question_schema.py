dcf263d9356958734991b190f51fcb4a0197731a
김세현 <dnjfdid14@naver.com>
2022-12-08 22:00:51 +0900

    ⚡️ :: 질문 수정 기능

--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -32,3 +32,7 @@ class QuestionCreate(BaseModel):
 class QuestionList(BaseModel):
     total: int = 0
     question_list: list[Question] = []
+
+
+class QuestionUpdate(QuestionCreate):
+    question_id: int
