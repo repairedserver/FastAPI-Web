448aa31b19df5df02b6d6d95597d0f6707e16f08
김세현 <dnjfdid14@naver.com>
2022-12-08 22:15:14 +0900

    ⚡️ :: 답변 수정

--- domain/answer/answer_router.py
+++ domain/answer/answer_router.py
@@ -32,3 +32,18 @@ def answer_create(question_id: int,
 def answer_detail(answer_id: int, db: Session = Depends(get_db)):
     answer = answer_crud.get_answer(db, answer_id=answer_id)
     return answer
+
+
+@router.put("/update", status_code=status.HTTP_204_NO_CONTENT)
+def answer_update(_answer_update: answer_schema.AnswerUpdate,
+                  db: Session = Depends(get_db),
+                  current_user: User = Depends(get_current_user)):
+    db_answer = answer_crud.get_answer(db, answer_id=_answer_update.answer_id)
+    if not db_answer:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="데이터를 찾을수 없습니다.")
+    if current_user.id != db_answer.user.id:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="삭제 권한이 없습니다.")
+    answer_crud.update_answer(db=db, db_answer=db_answer,
+                              answer_update=_answer_update)
