816d78f368fd3e916449e2686f408695ed52e52b
김세현 <dnjfdid14@naver.com>
2022-12-22 15:40:08 +0900

    ⚡️ :: 질문 투표

--- models.py
+++ models.py
@@ -10,6 +10,13 @@ question_voter = Table(
     Column('question_id', Integer, ForeignKey('question.id'), primary_key=True)
 )
 
+answer_voter = Table(
+    'answer_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('answer_id', Integer, ForeignKey('answer.id'), primary_key=True)
+)
+
 
 class User(Base):
     __tablename__ = "user"
@@ -41,3 +48,4 @@ class Answer(Base):
     question_id = Column(Integer, ForeignKey("question.id"))
     question = relationship("Question", backref="answers")
     modify_date = Column(DateTime, nullable=True)
+    voter = relationship('User', secondary=answer_voter, backref='answer_voters')
