4c1179eeeb1c4c0949620778b2fe879622d9790b
김세현 <dnjfdid14@naver.com>
2022-12-25 14:13:40 +0900

    ⚡️ :: 질문 검색

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,16 +1,28 @@
-from models import Question, User
+from models import Question, User, Answer
 from sqlalchemy.orm import Session
 from datetime import datetime
 
 from domain.question.question_schema import QuestionCreate, QuestionUpdate
 
 
-def get_question_list(db: Session, skip: int = 0, limit: int = 10):
-    _question_list = db.query(Question)\
-        .order_by(Question.create_date.desc())
-
-    total = _question_list.count()
-    question_list = _question_list.offset(skip).limit(limit).all()
+def get_question_list(db: Session, skip: int = 0, limit: int = 10, keyword: str = ''):
+    question_list = db.query(Question)
+    if keyword:
+        search = '%%{}%%'.format(keyword)
+        sub_query = db.query(Answer.question_id, Answer.content, User.username) \
+            .outerjoin(User, Answer.user_id == User.id).subquery()
+        question_list = question_list \
+            .outerjoin(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(Question.subject.ilike(search) |  # 질문 제목
+                    Question.content.ilike(search) |  # 질문 내용
+                    User.username.ilike(search) |  # 질문 작성자
+                    sub_query.c.content.ilike(search) |  # 답변 내용
+                    sub_query.c.username.ilike(search)  # 답변 작성자
+                    )
+    total = question_list.distinct().count()
+    question_list = question_list.order_by(Question.create_date.desc()) \
+        .offset(skip).limit(limit).distinct().all()
     return total, question_list
 
 
